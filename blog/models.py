ef6c7df224f7a08459f8449829410340d75577eb
Umaru Lamin <ulamin97@gmail.com>
2022-01-20 14:54:45 -0800

    latest commit on blog post project

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset() \
+                    .filter(status='published')
+
 class Post (models.Model):
+
+
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
@@ -22,6 +31,18 @@ class Post (models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+    #published = models.DateTimeField(auto_now=True)
+
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
+
+
 
     class Meta:
         ordering = ('-publish',)
